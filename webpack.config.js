51052b0b2c2ec5b891c129a0dbb9173fbfd02e3e
wmaqingbo <wmaqingbo@163.com>
2018-11-04 11:01:49 +0800

    添加了常用的插件和loader；

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path')
 const UglifyPlugin = require('uglifyjs-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
@@ -7,12 +9,31 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
+  devServer: {
+    port: 8084,
+    hot: true
+  },
   module: {
     rules: [
       {
         test: /\.jsx?/,
         include: [path.resolve(__dirname, 'src')],
         use: 'babel-loader'
+      }, {
+        test: /\.less$/,
+        // 因为这个插件需要干涉模块转换的内容，所以需要使用它对应的 loader
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'less-loader']
+        })
+      }, {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {}
+          },
+        ],
       }
     ]
   },
@@ -26,6 +47,14 @@ module.exports = {
   },
   plugins: [
     new UglifyPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'webpack-learn',
+      filename: 'index.html',
+      hash: true,
+      inject: 'head',
+      favicon: './favicon.ico'
+    }),
+    new ExtractTextPlugin('index.css')
     // 使用 uglifyjs-webpack-plugin 来压缩 JS 代码
     // 如果你留意了我们一开始直接使用 webpack 构建的结果，你会发现默认已经使用了 JS 代码压缩的插件
     // 这其实也是我们命令中的 --mode production 的效果，后续的小节会介绍 webpack 的 mode 参数
